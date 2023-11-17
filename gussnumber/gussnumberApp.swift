a80610418658d59e2e38ab801de2c7bf89f5a2f9
yoyoandop <yoyoandop@gmail.com>
2023-11-17 21:17:41 +0800

    first

---
+++ gussnumber/gussnumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  gussnumberApp.swift
+//  gussnumber
+//
+//  Created by AIR on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct gussnumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
